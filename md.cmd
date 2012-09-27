9197efd91ac046146d318d922669c7e848a46fb6
Arnie97 <arnie97@gmail.com>
2012-09-27 11:11:10 +0800

    Include a test suite generator.

---
+++ md.cmd
@@ -0,0 +1,8 @@
+@echo off
+echo. >"[兰兰迷路记.第四季].[SumiSora][The_Story_of_Lost_Ranran][01][720p][x264_aac][GB](32E4D52F).rmvb
+echo. >"[兰兰迷路记.第四季].[SumiSora&amp;MAGI_ATELIER&amp;CASO][The_Story_of_Lost_Ranran][02][2160p][H.264][Big5](D1F598).rmvb
+echo. >"[兰兰迷路记.第四季].[SumiSora&MAGI_ATELIER&CASO][The_Story_of_Lost_Ranran][03][848x480][x264_mp3][cn](19960416).rmvb
+echo. >"[兰兰迷路记.第四季].[Ktxp&SumiSora][The_Story_of_Lost_Ranran](www.fuck.org)[04v2][480p][RV10][x264][flac][jp](9A25B3F0).rmvb
+echo. >"[兰兰迷路记.第四季].[CASO&POPGO&DMG][The_Story_of_Lost_Ranran][05][jump][DVDRip][x264_aac]_baofeng.rmvb
+echo. >"[兰兰迷路记.第四季].[SumiSora&MAGI_ATELIER&CASO][The_Story_of_Lost_Ranran][06][GB][720p][x264_aac](32E4D52F).rmvb
+echo. >"[兰兰迷路记.第四季].[SumiSora&Ktxp][The_Story_of_Lost_Ranran][DVDRip][Vol.3][SP][GB_BIG5][X264_AC3](E6FC5957).mkv
