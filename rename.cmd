b6d0e052c5cecaa3ccaa80eb4ff945cb5f95ffbf
Arnie97 <arnie97@gmail.com>
2015-12-18 17:20:19 +0800

    Type Zero.

---
+++ rename.cmd
@@ -0,0 +1,9 @@
+dir /b >rename.csv & start rename.csv
+
+REM Copy the column in the list created.
+REM Manually replace filenames in the new column.
+REM Create a column filled with "ren ".
+REM Save as comma separated value file with extention ".cmd".
+REM Right click to edit it in notepad.
+REM Replace 'ren ,' with 'ren "', ',\r\n' with '"\r\n' and ','' with '" "'.
+REM Run it.
